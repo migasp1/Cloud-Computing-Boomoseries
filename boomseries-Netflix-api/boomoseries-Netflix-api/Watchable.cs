90fccf9a822a3d84e7778cba08a9c2cca95daf1f
Filipe Mota <filipe.miguel.mota2000@outlook.com>
2022-03-27 23:54:29 +0100

    Netflix Microservice finished

---
+++ boomseries-Netflix-api/boomoseries-Netflix-api/Watchable.cs
@@ -0,0 +1,11 @@
+﻿namespace boomoseries_Netflix_api
+{
+    public class watchable
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Type { get; set; } = string.Empty;
+        public string Date { get; set; } = string.Empty;
+        public float Rating { get; set; }
+    }
+}
