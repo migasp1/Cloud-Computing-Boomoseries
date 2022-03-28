b95d6b8467fa061f3a67376bb72f1e3ac333ada7
Filipe Mota <filipe.miguel.mota2000@outlook.com>
2022-03-29 00:25:53 +0100

    Disney microservice DTO change

---
+++ bomoseries-Disney-api/bomoseries-Disney-api/Mapper/DisneyMapper.cs
@@ -0,0 +1,21 @@
+﻿using bomoseries_Disney_api.DTOs;
+using boomoseries_Disney_api;
+
+namespace bomoseries_Disney_api.Mapper
+{
+    public class DisneyMapper
+    {
+        public static WatchableDTO MapToDTO(Watchable watchable)
+        {
+            return new WatchableDTO()
+            {
+                Id = watchable.Id,
+                Title = watchable.Title,
+                Rating = watchable.Rating,
+                Date = watchable.Date,
+                Type = watchable.Type,
+                Platform = "Netflix"
+            };
+        }
+    }
+}
