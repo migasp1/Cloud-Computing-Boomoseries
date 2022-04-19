7128105479356d7bcd4344d50f05d2e15d30720e
Filipe Mota <filipe.miguel.mota2000@outlook.com>
2022-04-19 23:32:17 +0100

    Restore mistake

--- boomoseries-Movies-api/boomoseries-Movies-api/Helpers/URLHelper.cs
+++ boomoseries-Movies-api/boomoseries-Movies-api/Helpers/URLHelper.cs
@@ -2,9 +2,9 @@
 {
     public class URLHelper
     {
-        private static readonly string NetflixBaseUrl = "http://25.95.87.113:5000/api/v1/Netflix/movies";
-        private static readonly string DisneyPlusBaseUrl = "http://25.95.87.113:5004/api/v1/Disney/movies";
-        private static readonly string AmazonPrimeBaseUrl = "http://25.95.87.113:5006/api/v1/Amazon/movies";
+        private static readonly string NetflixBaseUrl = "http://localhost:5000/api/v1/Netflix/movies";
+        private static readonly string DisneyPlusBaseUrl = "http://localhost:5004/api/v1/Disney/movies";
+        private static readonly string AmazonPrimeBaseUrl = "http://localhost:5006/api/v1/Amazon/movies";
 
         public static string[] GetMicroservicesBaseURL()
         {
