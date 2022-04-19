d13c1ebb3569ec5ddbf7e11357aae95474444313
Filipe Mota <filipe.miguel.mota2000@outlook.com>
2022-04-20 00:34:34 +0100

    change port for  docker

--- boomoseries-Search-api/boomoseries-Search-api/Helpers/URLHelper.cs
+++ boomoseries-Search-api/boomoseries-Search-api/Helpers/URLHelper.cs
@@ -2,9 +2,9 @@
 {
     public class URLHelper
     {
-            private static readonly string MoviesBaseUrl = "https://localhost:5009/api/v1/Movies";
-            private static readonly string SeriesBaseUrl = "https://localhost:50017/api/v1/Series";
-            private static readonly string BooksBaseUrl = "https://localhost:5015/api/v1/Books";
+            private static readonly string MoviesBaseUrl = "http://localhost:5008/api/v1/Movies";
+            private static readonly string SeriesBaseUrl = "http://localhost:5016/api/v1/Series";
+            private static readonly string BooksBaseUrl = "http://localhost:5014/api/v1/Books";
 
             public static string[] GetMicroservicesBaseURL()
             {
