cc0924f958ccb963bb553ff8ef301b877129dc5e
João Cristo <joao.pa.cristo@gmail.com>
2022-04-21 00:22:35 +0100

    little change in url

--- boomoseries-Movies-api/boomoseries-Movies-api/Helpers/URLHelper.cs
+++ boomoseries-Movies-api/boomoseries-Movies-api/Helpers/URLHelper.cs
@@ -3,7 +3,6 @@
     public class URLHelper
     {
         private static readonly string NetflixBaseUrl = "http://host.docker.internal:5000/api/v1/Netflix/movies";
-        private static readonly string IMDBBaseUrl = "http://host.docker.internal:5002/api/v1/IMDB/movies";
         private static readonly string DisneyPlusBaseUrl = "http://host.docker.internal:5004/api/v1/Disney/movies";
         private static readonly string AmazonPrimeBaseUrl = "http://host.docker.internal:5006/api/v1/Amazon/movies";
 
