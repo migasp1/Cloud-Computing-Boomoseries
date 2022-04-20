a355dce34ad009a5f42e6c853fcc1096a3664f10
João Cristo <joao.pa.cristo@gmail.com>
2022-04-21 00:59:32 +0100

    docker finnalized. 🥳🥳

--- boomoseries-OrchAuth-api/boomoseries-OrchAuth-api/Helpers/URLHelper.cs
+++ boomoseries-OrchAuth-api/boomoseries-OrchAuth-api/Helpers/URLHelper.cs
@@ -2,9 +2,9 @@
 {
     public class URLHelper
     {
-        public static readonly string _baseUsersMicroserviceURL = "https://localhost:5021/Users";
-        public static readonly string _baseUserPreferencesMicroserviceURL = "https://localhost:5024/UserPreferences/Favorites";
-        public static readonly string _baseSearchMicroserviceURL = "http://localhost:5018/api/v1/Search";
+        public static readonly string _baseUsersMicroserviceURL = "http://host.docker.internal:5020/Users";
+        public static readonly string _baseUserPreferencesMicroserviceURL = "http://host.docker.internal:5024/UserPreferences/Favorites";
+        public static readonly string _baseSearchMicroserviceURL = "http://host.docker.internal:5018/api/v1/Search";
 
     }
 }
